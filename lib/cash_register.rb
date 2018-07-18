06634674c488e3de1e12b9457d61d2e0b681bd19
Sarah Jacob <smjacob91@gmail.com>
2018-07-18 19:43:56 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -13,7 +13,12 @@ class CashRegister
   
   def apply_discount
    @total = ( 1 - (@discount.to_f/100))*@total
+   return "After the discounnt, the total comes to $#{@total}."
   end
   
 end
 
+ it 'returns success message with updated total' do
+        cash_register_with_discount.add_item("macbook air", 1000)
+        expect(cash_register_with_discount.apply_discount).to eq("After the discount, the total comes to $800.")
+      end
