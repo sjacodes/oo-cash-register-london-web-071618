11360d546c4ffea29b7a0b0d19c62f8cde93df38
Sarah Jacob <smjacob91@gmail.com>
2018-07-18 20:29:44 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,21 +1,18 @@
 class CashRegister
   
-  attr_accessor :total, :discount, :quantity
+  attr_accessor :total, :discount, :quantity :price
   
   def initialize(discount = 0)
     @total = 0
     @discount=discount
-    
     @all=[]  
     
   end
    
-   
-  
   def add_item(title, price, quantity = 1)
     @total = @total + (price*quantity)
     i=0
-    while i< quantity do
+    while i< quantity do 
       @all << title
       i += 1
     end
@@ -32,6 +29,10 @@ class CashRegister
   
   def items
     @all
+  end
+  
+  def void_last_transaction
+    @total = @total - self.price
   end
   
 end
