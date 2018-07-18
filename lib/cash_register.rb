ca2ed807f56f384a527aeb21b235fa97139707ed
Sarah Jacob <smjacob91@gmail.com>
2018-07-18 19:40:27 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -15,30 +15,5 @@ class CashRegister
    @total = ( 1 - (@discount.to_f/100))*@total
   end
   
-  
-  
 end
 
-
-
-
-
-# describe '#apply_discount' do
-#     context 'the cash register was initialized with an employee discount' do
-#       it 'applies the discount to the total price' do
-#         cash_register_with_discount.add_item("macbook air", 1000)
-#         cash_register_with_discount.apply_discount
-#         expect(cash_register_with_discount.total).to eq(800)
-#       end
-
-#       it 'returns success message with updated total' do
-#         cash_register_with_discount.add_item("macbook air", 1000)
-#         expect(cash_register_with_discount.apply_discount).to eq("After the discount, the total comes to $800.")
-#       end
-
-#       it 'reduces the total' do
-#         cash_register.total = 0
-#         cash_register_with_discount.add_item("macbook air", 1000)
-#         expect{cash_register_with_discount.apply_discount}.to change{cash_register_with_discount.total}.by(-200)
-#       end
-#     end
